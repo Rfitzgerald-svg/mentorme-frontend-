1c8b51a71bddbca9733e629731e734fd9c606d48
Rfitzgerald-svg <russell.fitzgerald.2025@gmail.com>
2025-06-01 00:31:13 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,1 @@
-
-import React from "react";
-
-export default function MentorshipOnboarding() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>MentorMe</h1>
-      <p>The full frontend logic is integrated and ready to go.</p>
-    </div>
-  );
-}
+{canvas_app_code}
