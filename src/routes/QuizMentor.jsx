29702213068b0330ca7674ae96cbb531d378bf6a
Rfitzgerald-svg <russell.fitzgerald.2025@gmail.com>
2025-06-02 00:17:17 -0400

    Create QuizMentor.jsx

---
+++ src/routes/QuizMentor.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function QuizMentor() {
+  return (
+    <div className="p-8 max-w-md mx-auto">
+      <h1 className="text-xl font-bold mb-4">Mentor Quiz</h1>
+      <p>This page will show the mentor onboarding quiz.</p>
+    </div>
+  );
+}
