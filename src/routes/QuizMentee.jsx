452b50b500d5afcfbe7c85d32a93b5689cd5f625
Rfitzgerald-svg <russell.fitzgerald.2025@gmail.com>
2025-06-02 00:17:53 -0400

    Create QuizMentee.jsx

---
+++ src/routes/QuizMentee.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function QuizMentee() {
+  return (
+    <div className="p-8 max-w-md mx-auto">
+      <h1 className="text-xl font-bold mb-4">Mentee Quiz</h1>
+      <p>This page will show the mentee onboarding quiz.</p>
+    </div>
+  );
+}
