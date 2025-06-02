45e54cf5661ac442122926286332c7047bbca89f
Rfitzgerald-svg <russell.fitzgerald.2025@gmail.com>
2025-06-02 10:42:27 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="812787131086-s525jstrmmppqqnvkagme8tshs3chtm6.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
