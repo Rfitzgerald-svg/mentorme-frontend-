e17d71f478816770ab0be8e1b8572cf8e52b2b1b
Rfitzgerald-svg <russell.fitzgerald.2025@gmail.com>
2025-06-08 19:19:09 -0400

    Restore /register and /register/email routes to test signup flow stability

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./routes/Landing.jsx";
+import Register from "./routes/Register.jsx";
+import RegisterEmail from "./routes/RegisterEmail.jsx";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register/email" element={<RegisterEmail />} />
       </Routes>
     </Router>
   );
