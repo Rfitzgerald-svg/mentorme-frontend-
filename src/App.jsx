8d283631dff075888d0feb196671228882632f98
Rfitzgerald-svg <russell.fitzgerald.2025@gmail.com>
2025-06-08 19:20:57 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./routes/Landing.jsx";
-import Register from "./routes/Register.jsx";
 import RegisterEmail from "./routes/RegisterEmail.jsx";
 
 export default function App() {
@@ -9,7 +8,6 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/register/email" element={<RegisterEmail />} />
       </Routes>
     </Router>
