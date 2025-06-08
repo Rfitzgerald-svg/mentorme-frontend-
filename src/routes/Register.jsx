0183402f3c5156b46822372906484e3752f96497
Rfitzgerald-svg <russell.fitzgerald.2025@gmail.com>
2025-06-08 18:49:18 -0400

    Update Register.jsx

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -20,20 +20,16 @@ export default function Register() {
       <div className="register-right">
         <div className="form-container">
           <h2>Get access to mentorship that actually matters.</h2>
-
           <button className="google-btn" onClick={handleGoogleLogin}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google icon" />
             Sign up with Google
           </button>
-
           <div className="or-divider">
             <hr /><span>OR</span><hr />
           </div>
-
           <button className="email-btn" onClick={handleEmailSignup}>
             Sign up with email
           </button>
-
           <p className="existing-account">
             Already have an account? <a href="/login">Log in</a>
           </p>
