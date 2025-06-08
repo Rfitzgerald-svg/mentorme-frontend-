fe3aeac2b1b3ab78099d56f7dd020ecb85dce0d1
Rfitzgerald-svg <russell.fitzgerald.2025@gmail.com>
2025-06-08 01:12:03 -0400

    Fix: Switch from HashRouter to BrowserRouter to enable routing to /register on Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./routes/Register.jsx";
 import Login from "./routes/Login.jsx";
 import QuizMentor from "./routes/QuizMentor.jsx";
