45d793d1e34815000a03edfb44d4b6386b5d0b14
Rfitzgerald-svg <russell.fitzgerald.2025@gmail.com>
2025-06-14 16:45:26 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import RegisterEmail from "./routes/RegisterEmail.jsx";
 import Login from "./routes/Login.jsx";
 import QuizMentor from "./routes/QuizMentor.jsx";
 import QuizMentee from "./routes/QuizMentee.jsx";
+import QuizReview from "./routes/QuizReview.jsx";
+import ChooseRole from "./routes/ChooseRole.jsx";
 import Dashboard from "./routes/Dashboard.jsx";
 import ChatRoom from "./routes/ChatRoom.jsx";
 import CalendarPage from "./routes/CalendarPage.jsx";
@@ -29,15 +31,17 @@ export default function App() {
         <Route path="/register/email" element={<RegisterEmail />} />
         <Route path="/login" element={<Login />} />
         <Route path="/demo" element={<Demo />} />
+        <Route path="/choose-role" element={<ChooseRole />} />
         <Route path="/quiz/mentor" element={<QuizMentor />} />
         <Route path="/quiz/mentee" element={<QuizMentee />} />
+        <Route path="/quiz/review" element={<QuizReview />} />
         <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
         <Route path="/network" element={<Layout><MyNetwork /></Layout>} />
         <Route path="/bot" element={<Layout><Bot /></Layout>} />
         <Route path="/protips" element={<Layout><ProTips /></Layout>} />
         <Route path="/admin" element={<Layout><AdminPanel /></Layout>} />
         <Route path="/profile" element={<Layout><Profile /></Layout>} />
-        <Route path="/profile/:username" element={<ProfilePublic />} />
+        <Route path="/profilepublic" element={<ProfilePublic />} />
         <Route path="/chat/:matchId" element={<ChatRoom />} />
         <Route path="/calendar" element={<CalendarPage />} />
       </Routes>
